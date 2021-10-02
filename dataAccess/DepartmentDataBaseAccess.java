ad7812989432e493cde81af8d38103d6ac01411d
zahra morovati <90718563+zahraMorovati@users.noreply.github.com>
2021-10-02 20:49:12 +0330

    add save method to department database access

--- dataAccess/DepartmentDataBaseAccess.java
+++ dataAccess/DepartmentDataBaseAccess.java
@@ -12,6 +12,18 @@ public class DepartmentDataBaseAccess {
         Class.forName("com.mysql.cj.jdbc.Driver");
         connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/maktab_58", "root", "password");
     }
+    
+    public void save(Department d) throws SQLException {
+
+        if (connection != null) {
+            Statement statement = connection.createStatement();
+            String sqlQuery = String.format("INSERT INTO department(Department_name,phon) values ('%s','%s')", d.getName(), d.getPhone());
+            statement.executeUpdate(sqlQuery);
+            System.out.println("\u001B[32m" + "operation done successfully!" + "\u001B[0m");
+        } else {
+            System.out.println("cannot access to database");
+        }
+    }
 
     
 }
